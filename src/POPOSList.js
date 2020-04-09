f39061d13f85fb231b6fe494c821add6e04e4f2d
ClaudiaMun <claudia.munozcortes@students.dominican.edu>
2020-04-09 12:54:15 -0700

    props

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -11,31 +11,34 @@ function POPOSList() {
       address="50 California St."
       image="50-california-st.jpg"
     />
-      <POPOSSpace
+    <POPOSSpace
       name="100 Pine Street"
       address="100 Pine St."
-      image="100=pine.jpg"/>
+      image="100-pine.jpg"
+    />
 
-      <POPOSSpace
+    <POPOSSpace
       name="101 California Street"
       address="101 California"
-      image="101-california.jpg" />
+      image="101-california.jpg"
+    />
 
-      <POPOSSpace
+    <POPOSSpace
       name="343 Sansome Roof Garden"
       address="343 Sansome St."
-      image="343-sansome-roof-garden.jpg" />
+      image="343-sansome-roof-garden.jpg"
+    />
 
-      <POPOSSpace
+    <POPOSSpace
         name="525 Market Street Plaza"
         address="525 Market St."
         image="525-market-street-plaza.jpg"
-      />
-      <POPOSSpace
+    />
+    <POPOSSpace
         name="Citigroup Center"
         address="1 Sansome St."
         image="citigroup-center.jpg"
-      />
+    />
     </div>
   )
 }
