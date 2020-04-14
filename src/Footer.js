022c3ba758b8cead7a120b3c66ad30e410c26105
ClaudiaMun <claudia.munozcortes@students.dominican.edu>
2020-04-14 13:17:53 -0700

    dynamic content and footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Footer.css';
 
 
+
 function Footer() {
   return (
     <div className="Footer">
@@ -9,10 +10,12 @@ function Footer() {
         <h1>Contact us</h1>
         <p>Swing by for a cup of coffee, or leave us a message:</p>
         <p>sfpopos@popos.org</p>
-        <h3>SF copyright 2020</h3>
+        <h3>SF copyright</h3>
       </footer>
     </div>
   )
 }
+
+
 
 export default Footer
